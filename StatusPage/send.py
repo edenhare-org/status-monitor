4931166a3225b1671ed4ff137b1c8e842128219c
chare@ios <labrlearning@gmail.com>
2021-04-27 22:47:19 -0500

    correct message PATCH

--- StatusPage/send.py
+++ StatusPage/send.py
@@ -1,57 +1,75 @@
 import logging
-__version__="1.0.0"
-__author__="chris.hare@icloud.com"
+import urllib3
+import json
+from .exceptions import Error
+from .exceptions import CreatePoolManagerFailure
+from .exceptions import RequestError
+from .exceptions import HttpRequestError
+try:  # load the certifi module for SSL certificate validation
+    import certifi
+except Exception:  # load failed, so initialize the connecton pool appropriately
+    no_certifi_load = True
+else:
+    no_certifi_load = False
+
+__version__ = "1.2.0"
+__author__ = "chris.hare@icloud.com"
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 logger.info("%s Module Version %s/%s", __name__, __version__, __author__)
 
+statusNames = ("operational", "under_maintenance", "degraded_performance",
+               "partial_outage", "major_outage")
 
-def status(**kwargs):
+if no_certifi_load is True:  # initialize the Pool Manager
+    logging.warning("certifi is not available.")
+    try:
+        pool = urllib3.PoolManager()
+    except Exception as e:
+        raise ConnectionError(e)
+else:  # load certifi with the Pool Manager
+    logging.debug("certifi loaded")
+    try:
+        pool = urllib3.PoolManager(ca_certs=certifi.where())
+    except Exception as e:
+        raise ConnectionError(e)
+
+
+def status(event):
     """use the update_component api to indicate the endpoint is operational"""
-    
-    logger.debug("kwargs = %s", kwargs)
-    
-    if kwargs.get('Status') not in ("operational", "under_maintenance", "degraded_performance", "partial_outage", "major_outage"):
-        logger.error("invalid status: %s", kwargs.get('Status'))
+
+    logger.debug("event = %s", event)
+
+    if event.get('url', None) is None:
+        raise AttributeError("url not specified")
+
+    if event.get('status') not in statusNames:
+        logger.error("invalid status: %s", event.get('status'))
         raise ValueError("invalid status")
-#    
-#    {
-#  "component": {
-#    "description": "string",
-#    "status": "operational",
-#    "name": "string",
-#    "only_show_if_degraded": true,
-#    "group_id": "string",
-#    "showcase": true,
-#    "start_date": "2021-04-24"
-#  }
-#}
-#
-#    logger.debug("sending up message")
-#    messageBody = kwargs.get("MessageBody")
-#    component = kwargs.get("Component")
-#    alerts=kwargs.get('Alerts', False)
-#    send = kwargs.get('Send', False)
-#    status = kwargs.get('Status', 0)
-#    statusText = kwargs.get('Name', "Not Defined")
-#    rTime = kwargs.get('Time', 0)
-#    endpoint = kwargs.get('Endpoint', None)
-#    url = kwargs.get('Url', None)
-#    alertConfig=kwargs.get('AlertConfig', None)
-#    emailConfig = kwargs.get('EmailConfig', None)
-#
-#    msg = EmailMessage()
-#    msg["From"] = emailConfig.get('from')
-#
-#    msg["Subject"] = "UP"
-#    msg["To"] = component
-#    msg.set_content(messageBody)
-#    kwargs['Message'] = msg
-#    kwargs['MailTo'] = component
-#    try:
-#        emaillib.send(**kwargs)
-#    except Exception as e:
-#        logger.error("could not send email: %s", e)
-#        raise Exception from e
-#
+
+    apiUrl = f"{event.get('baseUrl')}/pages/{event.get('pageid')}/components/{event.get('componentid')}"
+
+    fields = {
+        'component': {
+            'status': event.get('status'),
+        }
+    }
+
+    try:
+        response = pool.request(
+            'PATCH',
+            apiUrl,
+            body=json.dumps(fields),
+            headers={"Authorization": f"OAuth {event.get('apikey')}"},
+            retries=int(event.get('retries', 3)),
+            timeout=float(event.get('timeout', 3)))
+    except Exception as e:
+        raise ConnectionError(e)
+
+    logger.debug(json.loads(response.data.decode("utf-8")))
+
+    return (response)
+
+
+# end
 
