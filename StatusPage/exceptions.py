37146df4edc278c82824e0a12441b8e032d76b3b
chare@ios <labrlearning@gmail.com>
2021-04-27 22:45:45 -0500

    create module exceprions

---
+++ StatusPage/exceptions.py
@@ -0,0 +1,21 @@
+# define Python user-defined exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class CreatePoolManagerFailure(Error):
+    """Raised when the input value is too small"""
+    pass
+
+
+class RequestError(Error):
+    """Raised when the input value is too large"""
+    pass
+
+
+class HttpRequestError(Error):
+    """Raised when the input value is too large"""
+    pass
+
+
