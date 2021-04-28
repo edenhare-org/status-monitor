88e5867a6347e21ae58cc01c02717ae6f392491d
chare@ios <labrlearning@gmail.com>
2021-04-27 22:47:38 -0500

    add importing exception files

--- CloudWatch/__init__.py
+++ CloudWatch/__init__.py
@@ -1,1 +1,6 @@
 from .metrics import put
+from .exceptions import Error
+from .exceptions import CreatePoolManagerFailure
+from .exceptions import RequestError
+from .exceptions import HttpRequestError
+from .exceptions import MetricPushFailure
--- StatusPage/__init__.py
+++ StatusPage/__init__.py
@@ -1,2 +1,6 @@
 from .send import status
+from .exceptions import Error
+from .exceptions import CreatePoolManagerFailure
+from .exceptions import RequestError
+from .exceptions import HttpRequestError
 
